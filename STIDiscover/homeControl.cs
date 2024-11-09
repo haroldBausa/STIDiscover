96bd6439c4a4fe5388bc996fc57be5b4812482f3
haroldBausa <152797065+haroldBausa@users.noreply.github.com>
2024-11-09 22:33:16 +0800

    Update

--- STIDiscover/homeControl.cs
+++ STIDiscover/homeControl.cs
@@ -16,5 +16,10 @@ namespace STIDiscover
         {
             InitializeComponent();
         }
+
+        private void guna2TileButton1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
